3e866b57bac684c742b9102a7781d19b02d045aa
Ceferov Orxan <114466519+CeferovOrxan@users.noreply.github.com>
2022-11-17 17:13:44 -0500

    Add files via upload

---
+++ rootkit_axtarma.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import os
+
+os.system ("apt-get install figlet")
+os.system("apt-get install chkrootkit -y")
+os.system("clear")
+os.system("figlet RootKit_Axtarma")
+print (""" 
+ (: Hello Guys :)
+ 
+""")
+
+os.system("chkrootkit")
